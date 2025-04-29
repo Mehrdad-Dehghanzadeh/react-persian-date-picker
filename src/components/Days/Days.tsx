fa2f7f8b0b2a5228b899cf3a80b3e3e7ea79ef4b
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2025-04-29 18:48:40 +0330

    chore: add number

--- src/components/Days/Days.tsx
+++ src/components/Days/Days.tsx
@@ -1,6 +1,8 @@
 import type { Props } from './types'
 import './style.scss'
+import { useRef } from 'react'
 
 export const Days: React.FC<Props> = () => {
+  const days = useRef<number[]>(new Array(30).fill(null).map((_el, index) => index + 1))
   return <div className="persian-date-picker-days"></div>
 }
--- src/components/Years/Years.tsx
+++ src/components/Years/Years.tsx
@@ -1,3 +1,15 @@
+import React, { useRef } from 'react'
+
 export const Years: React.FC = () => {
-  return <div className="persian-date-picker-years"></div>
+  const years = useRef<number[]>(
+    new Array(200).fill(null).map((_el, index) => index + 1250)
+  )
+
+  return (
+    <div className="persian-date-picker-years">
+      {years.current.map((el) => (
+        <span>{el}</span>
+      ))}
+    </div>
+  )
 }
