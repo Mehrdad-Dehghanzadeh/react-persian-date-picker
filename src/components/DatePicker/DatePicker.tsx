338c40311c09711c2feb86255650bcea255851e6
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2025-05-09 22:51:01 +0330

    chore: inm

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,11 +1,37 @@
+import type { TDate } from '@type/Date'
+import type { TValue } from '@type/Value'
 import { Days, Months, Years } from '../index'
+import { useMemo, useState } from 'react'
+import jalaali from 'jalaali-js'
+import { DatePickerContext } from '@contexts/DatePicker'
 
 export const DatePicker: React.FC = () => {
+  const [date, setDate] = useState<TDate | null>(null)
+
+  const value = useMemo<TValue>(() => {
+    let val = {
+      utc: '',
+      jalaali: ''
+    }
+
+    if (date?.year && date?.month && date?.day) {
+      const gregorian = jalaali.toGregorian(date.year, date.month, date.day)
+      val.utc = `${gregorian.gy}-${gregorian.gm}-${gregorian.gd}T00:00:00Z`
+      val.jalaali = `${date?.year}/${date?.month}/${date?.day}`
+    }
+
+    return val
+  }, [date])
+
+  const DatePickerContextValue = useMemo(() => ({ date, setDate }), [date])
+
   return (
     <div className="persian-date-picker-wrapper__inputs">
-      <Days />
-      <Months />
-      <Years />
+      <DatePickerContext.Provider value={DatePickerContextValue}>
+        <Days />
+        <Months />
+        <Years />
+      </DatePickerContext.Provider>
     </div>
   )
 }
--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,46 +1,18 @@
 import type { Props } from './types'
-import type { TDate } from '@type/Date'
-import type { TValue } from '@type/Value'
-import { useMemo, useState } from 'react'
-import { DatePickerContext } from '@contexts/DatePicker'
-import jalaali from 'jalaali-js'
 import { DatePicker } from '@components/DatePicker/DatePicker'
 import './style.scss'
 
 export const Wrapper: React.FC<Props> = () => {
-  const [date, setDate] = useState<TDate | null>(null)
-
-  const value = useMemo<TValue>(() => {
-    let val = {
-      utc: '',
-      jalaali: ''
-    }
-
-    if (date?.year && date?.month && date?.day) {
-      const gregorian = jalaali.toGregorian(date.year, date.month, date.day)
-      val.utc = `${gregorian.gy}-${gregorian.gm}-${gregorian.gd}T00:00:00Z`
-      val.jalaali = `${date?.year}/${date?.month}/${date?.day}`
-    }
-
-    return val
-  }, [date])
-
-  const DatePickerContextValue = useMemo(() => ({ date, setDate }), [date])
-
   return (
     <div className="persian-date-picker-wrapper">
-      <DatePickerContext.Provider value={DatePickerContextValue}>
-        <DatePicker />
-        
-        <div className="persian-date-picker__btns">
-          <button type="button" disabled={!(value.jalaali || value.utc)}>
-            تایید
-          </button>
-          <button type="button">بستن</button>
-        </div>
+      <DatePicker />
 
-        <div className=""></div>
-      </DatePickerContext.Provider>
+      <div className="persian-date-picker__btns">
+        <button type="button">تایید</button>
+        <button type="button">بستن</button>
+      </div>
+
+      <div className=""></div>
     </div>
   )
 }
